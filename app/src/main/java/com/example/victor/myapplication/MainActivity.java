9fa4cc76d1321daedf76f52b705154cb2c323b84
Victor Chen <vchen8761@gmail.com>
2017-12-30 14:01:30 -0500

    created editText and Button

--- app/src/main/java/com/example/victor/myapplication/MainActivity.java
+++ app/src/main/java/com/example/victor/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.victor.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+
+
 }
